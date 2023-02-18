4d3e36a90f74e716abe695bbf5f8452597efc336
melmln <melanymolinaverdun@gmail.com>
2023-02-17 21:26:45 -0300

    Finalizo diseño del Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,28 +6,37 @@ const Home = () => {
   return (
     <main className='home'>
         <div className="banner-container"></div>
+        <div className="black-line-container"></div>
+        <div className="white-wave-container"></div>
+                      {/* Cards */}
         <div className="cards-container">
-           <div className="left-card">
-              <p>¿Sabías que el hogar cuenta con 5 proyectos?</p>
-              <button className='conocelos-btn'>CONOCELOS</button>
-           </div>
-           <div className="central-card">
-              <p>Tu aporte es muy valioso para el hogar</p>
-              <button className='sumate-btn'>SUMATE</button>
-           </div>
-           <div className="right-card">
-              <p>¿Sabés cuales son los requisitos para adoptar?</p>
-              <button className='te-contamos-btn'>TE CONTAMOS</button>
-           </div>
+            <div className="left-card">
+                  <p>¿Sabías que el hogar cuenta con 5 proyectos?</p>
+                  <button className='conocelos-btn'>CONOCELOS</button>
+            </div>
+
+            <div className="central-card">
+                <p>Tu aporte es muy valioso para el hogar</p>
+                <button className='sumate-btn'>SUMATE</button>
+            </div>
+
+            <div className="right-card">
+                <p>¿Sabés cuales son los requisitos para adoptar?</p>
+                <button className='te-contamos-btn'>TE CONTAMOS</button>
+            </div> 
         </div>
+                 {/* Frase William Morris */}
           <div className="quote-container">
             <div className="quote-line-container">
-              <img src={whiteLine} alt="" />
-              <h2>Frase Lema WILLIAM CASE MORRIS</h2>
-              <img src={whiteLineLoop} alt="" />
+              <img className='line'
+              src={whiteLine} alt="line" />
+              <h3>Frase Lema WILLIAM CASE MORRIS</h3>
+              <img className='line-loop'
+              src={whiteLineLoop} alt="line-loop" />
             </div>
-              <span>-Fundador Hogar-</span>
-            <p className='quote-w.c.m'>
+            <span className='fundador-hogar'>-Fundador Hogar-</span>
+            
+            <p className='quote-william-morris'>
             "Pasaré por este mundo una sola vez. Si hay alguna palabra bondadosa que yo pueda pronunciar alguna noble acción
             que yo pueda efectuar diga yo esa palabra, haga yo esa acción ahora,  pues no pasaré más por aquí..."
             </p>
