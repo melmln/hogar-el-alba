968a431910f44f7700193e348b8ac4d53e8b5e17
Fede Krenn <fedekrenn@gmail.com>
2023-02-20 12:58:49 -0300

    Se termina sección contacto

--- src/pages/Contacto/Contacto.jsx
+++ src/pages/Contacto/Contacto.jsx
@@ -1,9 +1,46 @@
 import './Contacto.css'
+import locationMap from '../../assets/img/contacto/mapa-de-ubicacion.png'
 
 const Contacto = () => {
   return (
-    <main className="contacto">
-      <h1>Contacto Section</h1>
+    <main className='contact'>
+      <section className='contact-title'>
+        <h1>Contactanos</h1>
+        <h2>Escribinos para más información y visitanos en el hogar</h2>
+      </section>
+      <section>
+        <img src={locationMap} alt='Mapa de locación de Hogar El Alba' />
+      </section>
+      <section className='form-section'>
+        <form action='' method=''>
+          <h2>Envianos un mensaje</h2>
+          <div>
+            <input
+              type='text'
+              name='nombre'
+              id='nombre'
+              placeholder='Nombre'
+              required
+            />
+            <input
+              type='tel'
+              name='telefono'
+              id='telefono'
+              placeholder='Teléfono'
+              required
+            />
+          </div>
+          <input
+            type='email'
+            name='email'
+            id='email'
+            placeholder='Email'
+            required
+          />
+          <textarea name='mensaje' id='mensaje' cols='30' rows='10' placeholder='Mensaje'></textarea>
+          <input type='submit' value='Enviar' />
+        </form>
+      </section>
     </main>
   )
 }
