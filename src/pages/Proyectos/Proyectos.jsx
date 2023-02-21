dc5e3e0696abc50f611fcfc1fe4b2874246fc641
Fede Krenn <fedekrenn@gmail.com>
2023-02-21 15:18:23 -0300

    Se trabaja sobre las cards de proyectos

--- src/pages/Proyectos/Proyectos.jsx
+++ src/pages/Proyectos/Proyectos.jsx
@@ -1,9 +1,39 @@
 import './Proyectos.css'
+import background from '../../assets/img/proyectos/pies-y-manos-de-bebe.png'
+import proyects from '../../utils/proyectos.json'
 
 const Proyectos = () => {
   return (
     <main className='proyectos'>
-      <h1>Proyectos section</h1>
+      <section>
+        <img src={background} alt='Proyecto Amparo - Hogar El Alba' />
+      </section>
+      <section>
+        <h1>Nuestros Proyectos</h1>
+        <div className='card-container'>
+          {proyects.map((proyect) => {
+            const { id, title, description, img, bgColor } = proyect
+            return (
+              <div
+                key={id}
+                style={{ background: bgColor }}
+                className='proyect-card'
+              >
+                <div className='title'>
+                  <img
+                    src={require(`../../assets/icons/proyectos/${img}`)}
+                    alt={title}
+                  />
+                  <h2>
+                    Proyecto <span>{title}</span>
+                  </h2>
+                </div>
+                <p>{description}</p>
+              </div>
+            )
+          })}
+        </div>
+      </section>
     </main>
   )
 }
