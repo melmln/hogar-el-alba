4ca881442122643d768f4e3848fe345d5ce97b6b
melmln <melanymolinaverdun@gmail.com>
2023-02-24 03:38:25 -0300

    Avances responsive design en Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,44 +1,60 @@
 import './Home.css'
+/* assets */
 import whiteLine from '../../assets/img/home/white-line.png'
 import whiteLineLoop from '../../assets/img/home/white-line-loop.png'
+import whiteWave from '../../assets/img/home/white-wave.png'
+import yellowCard from '../../assets/img/home/yellow-card.png'
+import redCard from '../../assets/img/home/red-card.png'
+import blueCard from '../../assets/img/home/blue-card.png'
 
 const Home = () => {
   return (
     <main className='home'>
-        <div className="banner-container">
-           <div className="white-wave-container"></div>
-        </div>
 
+        <div className="banner-container">
+          <div className="white-wave-container">
+            <img className="white-wave" src={whiteWave}/>
+          </div>
+        </div>
         
                       {/* Cards */}
-        <div className="cards-container">
-        <div className="black-line-container"></div> 
-            <div className="left-card">
-                  <p>¿Sabías que el hogar cuenta con 5 proyectos?</p>
-                  <button className='conocelos-btn'>CONOCELOS</button>
-            </div>
+        <div className="home-cards-container">
 
-            <div className="central-card">
-                <p>Tu aporte es muy valioso para el hogar</p>
-                <button className='sumate-btn'>SUMATE</button>
-            </div>
+             <div className="home-card proyectos">
+                  <img src={yellowCard} alt="yellow-card" className="img-proyectos" />
+                  <div className="home-card-content">
+                      <p>¿Sabías que el hogar cuenta con 5 proyectos?</p>
+                      <button className='home-card-btn proyectos-btn'>CONOCELOS</button>
+                  </div>
+              </div>
 
-            <div className="right-card">
-                <p>¿Sabés cuales son los requisitos para adoptar?</p>
-                <button className='te-contamos-btn'>TE CONTAMOS</button>
-            </div> 
+              <div className="home-card donar">
+                  <img src={redCard} alt="red-card" className="img-proyectos" />
+                  <div className="home-card-content">
+                        <p>Tu aporte es muy valioso para el hogar</p>
+                        <button className='home-card-btn donar-btn'>SUMATE</button>
+                  </div>
+              </div>
+
+                <div className="home-card adoptar">
+                    <img src={blueCard} alt="blue-card" className="img-proyectos" />
+                    <div className="home-card-content">
+                        <p>¿Sabés cuales son los requisitos para adoptar?</p>
+                        <button className='home-card-btn adoptar-btn'>TE CONTAMOS</button>
+                    </div>
+                </div>  
         </div>
                  {/* Frase William Morris */}
           <div className="quote-container">
+
             <div className="quote-line-container">
-              <img className='line'
-              src={whiteLine} alt="line" />
-              <h3>Frase Lema WILLIAM CASE MORRIS</h3>
-              <img className='line-loop'
-              src={whiteLineLoop} alt="line-loop" />
+                <img className='line'
+                src={whiteLine} alt="line" />
+                <h3>Frase Lema WILLIAM CASE MORRIS</h3>
+                <img className='line-loop'
+                src={whiteLineLoop} alt="line-loop" />
             </div>
             <span className='fundador-hogar'>-Fundador Hogar-</span>
-            
             <p className='quote-william-morris'>
             "Pasaré por este mundo una sola vez. Si hay alguna palabra bondadosa que yo pueda pronunciar alguna noble acción
             que yo pueda efectuar diga yo esa palabra, haga yo esa acción ahora,  pues no pasaré más por aquí..."
