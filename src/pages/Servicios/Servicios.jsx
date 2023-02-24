38ee2127b31f2f295417c33545a3a9faa06f9dbb
melmln <melanymolinaverdun@gmail.com>
2023-02-24 00:28:24 -0300

    Finalizo responsive design y media queries de la sección Servicios

--- src/pages/Servicios/Servicios.jsx
+++ src/pages/Servicios/Servicios.jsx
@@ -19,19 +19,95 @@ const Servicios = () => {
             <h1 className='servicios-title'>Servicios que ofrecemos</h1>
           
           <div className="gallery-container">
-              <img src={zonaCarpas} alt="zona-de-carpas" />
-              <img src={eventosRecreativos} alt="eventos-recreativos" />
-              <img src={eventosDeportivos} alt="eventos-deportivos" />
-              <img src={eventosEmpresariales} alt="eventos-empresariales" />               
-              <img src={eventosSociales} alt="eventos-sociales" />                
-              <img src={gimnasio} alt="gimnasio" />                 
-              <img src={zonaPicnic} alt="zona-de-picnic" />
-              <img src={quincho} alt="quincho" />                
-              <img src={plaza} alt="plaza" />                  
-              <img src={pileta} alt="pileta" />                   
-              <img src={aireLibre} alt="eventos-al-aire-libre" />                 
-              <img src={campamento} alt="campamento-holanda" />       
-          </div>
+                 <div className="gallery-images">
+
+                    <div className="card-image-container">
+                        <div className="image-container">
+                          <img src={zonaCarpas} alt="zona-de-carpas" />
+                        </div>
+                        <div className="caption">Zona de carpas</div>
+                    </div>
+
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={eventosRecreativos} alt="eventos-recreativos" />
+                        </div>
+                        <div className="caption">Eventos recreativos</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={eventosDeportivos} alt="eventos-deportivos" />
+                        </div>
+                        <div className="caption">Eventos deportivos</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={eventosEmpresariales} alt="eventos-empresariales" />               
+                        </div>
+                        <div className="caption">Eventos empresariales</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={eventosSociales} alt="eventos-sociales" />                
+                        </div>
+                        <div className="caption">Eventos sociales</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={gimnasio} alt="gimnasio" />                 
+                        </div>
+                        <div className="caption">Gimnasio</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={zonaPicnic} alt="zona-de-picnic" />
+                        </div>
+                        <div className="caption">Zona de picnic</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={quincho} alt="quincho" />                
+                        </div>
+                        <div className="caption">Quincho</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={plaza} alt="plaza" />                  
+                        </div>
+                        <div className="caption">Plaza</div>
+                    </div>
+                  
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={pileta} alt="pileta" />                   
+                        </div>
+                        <div className="caption">Pileta</div>
+                    </div>
+
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={aireLibre} alt="eventos-al-aire-libre" />                 
+                        </div>
+                        <div className="caption">Eventos al aire libre</div>
+                    </div>
+
+                    <div className="card-image-container">
+                        <div className="image-container">
+                        <img src={campamento} alt="campamento-holanda" />  
+                        </div>
+                        <div className="caption">Campamento Holanda</div>
+                    </div>
+                  
+                </div>
+              </div>     
+
     </main>
   )
 }
