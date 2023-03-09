7bc2268045b83b2c9fb9b71cded61b581a6988df
melmln <melanymolinaverdun@gmail.com>
2023-03-09 19:42:20 -0300

    Estado activo fijo de las categorías en vista desktop y mobile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 import './Header.css'
-import { Link } from 'react-router-dom'
+import { Link} from 'react-router-dom'
 import { useState } from 'react';
 /* Assets */
 import logo from '../../../src/logo.png'
@@ -14,47 +14,62 @@ const Header = () => {
   const bmChange = () => {
     setBmOpen(!bmOpen)
   }
+   const [catgActive, setCatActive] = useState('')
 
-  const [catActive, setCatActive] = useState(false)
-
-  const categoryActive = () => {
-    setCatActive(!catActive)
+  const categoryActive = (category) => {
+    setCatActive(category)
   }
-
+ 
   return (
         <header>
             <nav>
+                                {/* Logo */}
+
                   <div className="logo-container">
                     <Link to="/">
                       <img src={logo} alt="logo" className="logo" />
                     </Link>
                   </div>
+
+                                {/* Categorías */}
+
                     <div className={`categories-container ${bmOpen && 'open'}`}>
                           <ul className="categories">
-                                  <li className={`category ${catActive && 'category-active'}`}
-                                  onClick={categoryActive}>
-                                    <Link to='/nosotros' >Nosotros</Link></li>
+                              <li 
+                                className={`category ${catgActive === 'nosotros-category' ? 'active' : ''}` }
+                                onClick={() => categoryActive('nosotros-category')}>
+                                <Link to='/nosotros' >Nosotros</Link>
+                              </li>
                                   
-                                  <li className="category"
-                                  onClick={categoryActive}>
-                                    <Link to='/proyectos'>Proyectos</Link></li>
+                              <li 
+                                className={`category ${catgActive === 'proyectos-category' ? 'active' : ''}` }
+                                onClick={() => categoryActive('proyectos-category')}>
+                                <Link to='/proyectos'>Proyectos</Link>
+                              </li>
                                   
-                                  <li className="category"
-                                  onClick={categoryActive}>
-                                    <Link to='/servicios'>Servicios</Link></li>
-                                  
-                                  <li className="category"
-                                  onClick={categoryActive}>
-                                    <Link to='/contacto'>Contacto</Link></li>
+                              <li 
+                               className={`category ${catgActive === 'servicios-category' ? 'active' : ''}` }
+                               onClick={() => categoryActive('servicios-category')}>
+                                <Link to='/servicios'>Servicios</Link>
+                              </li>
+                              
+                              <li  className={`category ${catgActive === 'contacto-category' ? 'active' : ''}` }
+                                onClick={() => categoryActive('contacto-category')}>
+                                <Link to='/contacto'>Contacto</Link>
+                              </li>
                           </ul>
                           
                           <Link to='/quiero-ayudar' className="donate-btn">QUIERO AYUDAR</Link>
                    </div>
 
+                                 {/* Burguer Menu */}
+
                     <div className={`bm-container ${bmOpen && 'open'}`} onClick={bmChange}>
                        <img src={bm} alt="burguer-menu" />
                     </div>
             </nav>
+                                {/* Fondo Header */}
+                                
             <div className="background-header-container">
               <img src={headerBackground} alt="background-header" />
             </div>
