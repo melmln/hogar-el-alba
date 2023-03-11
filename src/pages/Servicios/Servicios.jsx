f18c2bc1d92796838465d2f5e7fed6c53dceef1a
melmln <melanymolinaverdun@gmail.com>
2023-03-11 11:01:09 -0300

    Agrego archivo json con listado de imagenes y cambio estructura de Servicios con un mapeo

--- src/pages/Servicios/Servicios.jsx
+++ src/pages/Servicios/Servicios.jsx
@@ -1,4 +1,6 @@
 import './Servicios.css'
+import { useContext, useState } from 'react'
+import { CategoryContext } from '../../context/CategoryContext'
 /* imágenes */
 import zonaCarpas from '../../assets/img/servicios/zona-carpas.png'
 import eventosRecreativos from '../../assets/img/servicios/eventos-recreativos.png'
@@ -12,13 +14,12 @@ import plaza from '../../assets/img/servicios/plaza.png'
 import pileta from '../../assets/img/servicios/pileta.png'
 import aireLibre from '../../assets/img/servicios/aire-libre.png'
 import campamento from '../../assets/img/servicios/campamento.png'
-import { useContext, useState } from 'react'
-import { CategoryContext } from '../../context/CategoryContext'
+
+/* json */
+import galería from '../../utils/galería.json'
 
 const Servicios = () => {
   const { changeCategory } = useContext(CategoryContext)
-
-            
 
   return (
     <main className='servicios' onLoad={() => changeCategory('servicios')}>
@@ -26,89 +27,19 @@ const Servicios = () => {
 
       <div className='gallery-container'>
         <div className='gallery-images'>
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={zonaCarpas} alt='zona-de-carpas' />
-            </div>
-            <div className='caption'>Zona de carpas</div>
-          </div>
+          {
+            galería.map((images) => {
+              return (
+              <div key={images.id} className='card-image-container'>
+                <div className='image-container'>
+                  <img src={require(`../../assets/img/servicios/${images.img}`)} alt={images.title} />
+                </div>
+                <div className='caption'>{images.title}</div>
+              </div>
 
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={eventosRecreativos} alt='eventos-recreativos' />
-            </div>
-            <div className='caption'>Eventos recreativos</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={eventosDeportivos} alt='eventos-deportivos' />
-            </div>
-            <div className='caption'>Eventos deportivos</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={eventosEmpresariales} alt='eventos-empresariales' />
-            </div>
-            <div className='caption'>Eventos empresariales</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={eventosSociales} alt='eventos-sociales' />
-            </div>
-            <div className='caption'>Eventos sociales</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={gimnasio} alt='gimnasio' />
-            </div>
-            <div className='caption'>Gimnasio</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={zonaPicnic} alt='zona-de-picnic' />
-            </div>
-            <div className='caption'>Zona de picnic</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={quincho} alt='quincho' />
-            </div>
-            <div className='caption'>Quincho</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={plaza} alt='plaza' />
-            </div>
-            <div className='caption'>Plaza de juegos</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={pileta} alt='pileta' />
-            </div>
-            <div className='caption'>Pileta</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={aireLibre} alt='eventos-al-aire-libre' />
-            </div>
-            <div className='caption'>Eventos al aire libre</div>
-          </div>
-
-          <div className='card-image-container'>
-            <div className='image-container'>
-              <img src={campamento} alt='campamento-holanda' />
-            </div>
-            <div className='caption'>Campamento Holanda</div>
-          </div>
+              )
+            })
+          }
         </div>
       </div>
     </main>
